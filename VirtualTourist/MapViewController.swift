917653c4261a51d63a4357871ebeed978a079763
StefanClausen <stefanclaussen@me.com>
2017-07-12 11:50:08 +0100

    Add annotation on a long press

---
+++ VirtualTourist/MapViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by Stefan Claussen on 12/07/2017.
+//  Copyright © 2017 One foot after the other. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, UIGestureRecognizerDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureLongPressGestureRecognizer()
+    }
+    
+    func configureLongPressGestureRecognizer() {
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action:(#selector(longPress)))
+        gestureRecognizer.minimumPressDuration = 2.0
+        gestureRecognizer.delegate = self
+        
+        mapView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    func longPress(gestureRecognizer: UILongPressGestureRecognizer) {
+        let touchPoint = gestureRecognizer.location(ofTouch: 0, in: mapView)
+        let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinates
+        
+        mapView.addAnnotation(annotation)
+    }
+}
