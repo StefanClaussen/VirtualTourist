fd154859f519e945aa5a3e559d6580b1b8aa86ca
StefanClausen <stefanclaussen@me.com>
2017-07-14 11:54:07 +0100

    Add PhotoDataSource

---
+++ VirtualTourist/PhotoDataSource.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoDataSource.swift
+//  VirtualTourist
+//
+//  Created by Stefan Claussen on 14/07/2017.
+//  Copyright © 2017 One foot after the other. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        
+        return cell
+    }
+}
